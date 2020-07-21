60c655f49183ea3671c43b388bb20b0491944829
Zockedidock <joona.brueckner@gmail.com>
2020-07-22 00:49:05 +0200

    - added popup, removed test2.js

--- test.js
+++ test.js
@@ -1,13 +1,30 @@
-const speedValue = document.getElementById("speed-value");
-const speedUnits = document.getElementById("speed-units");
+const speedValue2 = document.getElementById("speed-value");
+const speedUnits2 = document.getElementById("speed-units");
+const message = document.getElementById("your-speed-message");
+const showMoreInfoT = document.getElementById("show-more-details-link");
+let divTest = document.getElementsByClassName("more-info-container");
+let num = null;
+let btn = document.createElement("BUTTON");
 
-switch (speedUnits) {
-    case "Mbps":
-        speedUnits.innerHTML = "Mb/s";
-    break;
-    case "Kbps":
-        speedUnits.innerHTML = "Kb/s";
-    break;
-}
+btn.innerHTML = "Convert";
+btn.className = "more-info-link";
+btn.style = "background-color: white; margin-left: 10px;"
+divTest[0].id = "test";
 
-speedValue.innerHTML = speedValue.innerHTML / 8;
+document.getElementById("test").style.display = "flex";
+
+btn.onclick = function(){
+    num = speedValue2.innerHTML / 8;
+    speedValue2.innerHTML = num.toFixed(1);
+    switch (speedUnits2.innerHTML) {
+        case "Mbps":
+            speedUnits2.innerHTML = "Mb/s";
+        break;
+        case "Kbps":
+            speedUnits2.innerHTML = "Kb/s";
+        break;
+    }
+    btn.style.display = "none";
+};
+
+document.getElementById("test").appendChild(btn);
