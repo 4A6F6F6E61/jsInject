6b4deff057269ed6218cf9a753482a09f31caa09
Zockedidock <joona.brueckner@gmail.com>
2020-07-22 11:22:50 +0200

    - Button hides if pressed aufter result - js looks better now

--- converter.js
+++ converter.js
@@ -1,30 +1,44 @@
-const speedValue2 = document.getElementById("speed-value");
-const speedUnits2 = document.getElementById("speed-units");
-const message = document.getElementById("your-speed-message");
-const showMoreInfoT = document.getElementById("show-more-details-link");
-let divTest = document.getElementsByClassName("more-info-container");
-let num = null;
-let btn = document.createElement("BUTTON");
+const speedValue2 = document.getElementById("speed-value")
+const speedUnits2 = document.getElementById("speed-units")
+const message = document.getElementById("your-speed-message")
+const showMoreInfoT = document.getElementById("show-more-details-link")
+const progressCircle = document.getElementById("speed-progress-indicator")
 
-btn.innerHTML = "Convert";
-btn.className = "more-info-link";
+let divTest = document.getElementsByClassName("more-info-container")
+let btn = document.createElement("BUTTON")
+let num = null
+
+btn.innerHTML = "Convert"
+btn.className = "more-info-link"
 btn.style = "background-color: white; margin-left: 10px;"
-divTest[0].id = "test";
+divTest[0].id = "test"
 
-document.getElementById("test").style.display = "flex";
+document.getElementById("test").style.display = "flex"
+document.getElementById("test").appendChild(btn)
 
 btn.onclick = function(){
-    num = speedValue2.innerHTML / 8;
-    speedValue2.innerHTML = num.toFixed(1);
-    switch (speedUnits2.innerHTML) {
-        case "Mbps":
-            speedUnits2.innerHTML = "Mb/s";
-        break;
-        case "Kbps":
-            speedUnits2.innerHTML = "Kb/s";
-        break;
+    if (progressCircle.classList.contains("in-progress")){
+        num = speedValue2.innerHTML / 8
+        speedValue2.innerHTML = num.toFixed(1)
+        switch (speedUnits2.innerHTML) {
+            case "Mbps":
+                speedUnits2.innerHTML = "Mb/s"
+            break
+            case "Kbps":
+                speedUnits2.innerHTML = "Kb/s"
+            break
+        }
+    } else {
+        num = speedValue2.innerHTML / 8
+        speedValue2.innerHTML = num.toFixed(1)
+        switch (speedUnits2.innerHTML) {
+            case "Mbps":
+                speedUnits2.innerHTML = "Mb/s"
+            break
+            case "Kbps":
+                speedUnits2.innerHTML = "Kb/s"
+            break
+        }
+        btn.style.display = "none"
     }
-    btn.style.display = "none";
-};
-
-document.getElementById("test").appendChild(btn);
+}
